4c8a554147f9e599c4a82ada1bbad8d4d53ad1c8
ambarish2012 <ambarish@shippable.com>
2017-05-10 14:38:38 -0700

    update docs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var readline = require('readline');
 var stream = require('stream');
 var _ = require('underscore');
 var async = require('async');
+const yaml = require('js-yaml');
+
 var searchJSON = [];
 var filePaths = [];
 
@@ -18,23 +20,28 @@ var headingCounter = 0;
 var listItemsCounter = 0;
 var paragraphCounter = 0;
 var codeCounter = 0;
-var currentHeading = null;
+var currentHeadingLink = null;
 
 renderer.heading = function (text, level) {
     _addHeading(text);
     return "";
 }
 
-
-
 renderer.paragraph = function (text) {
     if (text.match(/main_section/g) != null) {
         var arr = text.split(/\r?\n/);
 
-        gbag.data.main_section = arr[0].split(/:/)[1];
-        gbag.data.sub_section = arr[1].split(/:/)[1];
+        arr.forEach(function (sections) {
+            var parts = sections.split(/:/);
 
-        // gbag.data.sub_sub_section_section = arr[2].split(/:/)[1];
+            if (parts[0] === "main_section") {
+                gbag.data.main_section = parts[1];
+            } else if (parts[0] === "sub_section") {
+                gbag.data.sub_section = parts[1];
+            } if (parts[0] === "page_main_title") {
+                gbag.data.page_main_title = parts[1];
+            }
+        });
     } else {
         var paragraph = _removeTagsAndExtraWhiteSpace(text);
 
@@ -49,7 +56,7 @@ renderer.paragraph = function (text) {
                 var property = "paragraph" + paragraphCounter;
 
                 gbag.data.page_paragraphs[property] = paragraph;
-                gbag.data.paragraphs_link_map[property] = currentHeading;
+                gbag.data.paragraphs_link_map[property] = currentHeadingLink;
             }
         }
     }
@@ -64,7 +71,7 @@ renderer.listitem = function (text) {
         listItemsCounter++;
         var property = "list_item" + listItemsCounter;
         gbag.data.page_listitems[property] = list_item;
-        gbag.data.listitems_link_map[property] = currentHeading;
+        gbag.data.listitems_link_map[property] = currentHeadingLink;
     }
 
     return "";
@@ -76,75 +83,73 @@ renderer.code = function (code, language) {
     codeCounter++;
     var property = "code" + codeCounter;
     gbag.data.page_code_snippets[property] = code_snippet;
-    gbag.data.code_snippets_link_map[property] = currentHeading;
+    gbag.data.code_snippets_link_map[property] = currentHeadingLink;
 
     return "";
 }
 
-// var contents = "![console-view](https://github.com/devops-recipes/test-api-nouvola/raw/master/runshjob-console-view.png)";
-// marked(contents, { renderer: renderer });
+function parseDocsYml() {
+    glob("docs/sources/**/*.md", function (err, res) {
+        if (err)
+            console.log('Error while getting the files');
+        filePaths = res;
+        async.eachSeries(filePaths,
+            function (filePath, callback) {
 
-//glob("docs/sources/**/*.md", function(err, res) {
-glob("docs/sources/**/*.md", function(err, res) {
-    if (err)
-        console.log('Error while getting the files');
-    filePaths = res;
-    async.eachSeries(filePaths,
-        function (filePath, callback) {
+                headingCounter = 0;
+                listItemsCounter = 0;
+                paragraphCounter = 0;
+                codeCounter = 0;
 
-            headingCounter = 0;
-            listItemsCounter = 0;
-            paragraphCounter = 0;
-            codeCounter = 0;
+                var path = filePath;
+                var data = {
+                    page_name: '',
+                    main_section: '',
+                    sub_section: '',
+                    page_main_title: '',
+                    page_main_heading: null,
+                    page_headings: new Object(),
+                    page_paragraphs: new Object(),
+                    page_listitems: new Object(),
+                    page_code_snippets: new Object(),
+                    page_baselink: '',
+                    headings_link_map: new Object(),
+                    paragraphs_link_map: new Object(),
+                    listitems_link_map: new Object(),
+                    code_snippets_link_map: new Object()
+                };
 
-            var path = filePath;
-            var data = {
-                page_name: '',
-                main_section: '',
-                sub_section: '',
-                sub_sub_section: '',
-                page_main_heading: null,
-                page_headings: new Object(),
-                page_paragraphs: new Object(),
-                page_listitems: new Object(),
-                page_code_snippets: new Object(),
-                page_baselink: '',
-                headings_link_map: new Object(),
-                paragraphs_link_map: new Object(),
-                listitems_link_map: new Object(),
-                code_snippets_link_map: new Object()
-            };
+                data.page_name = path.replace('docs/sources/', '');
+                data.page_name = data.page_name.replace('.md', '');
+                data.page_baselink = 'http://rcdocs1.shippable.com/' + data.page_name;
 
-            data.page_name = path.replace('docs/sources/', '');
-            data.page_name = data.page_name.replace('.md', '');
-            data.page_baselink = 'http://rcdocs1.shippable.com/' + data.page_name;
+                var bag = {
+                    filePath: filePath,
+                    data: data
+                };
 
-            var bag = {
-                filePath: filePath,
-                data: data
-            };
+                async.series([
+                        _parseFileMD.bind(null, bag)
+                    ],
+                    function (err) {
+                        if (err)
+                            console.log('Error');
+                        return callback();
+                    }
+                );
+            },
+            function (err) {
+                if (err)
+                    console.log('failed');
 
-            async.series([
-                    _parseFileMD.bind(null, bag)
-                ],
-                function (err) {
-                    if (err)
-                        console.log('Error');
-                    return callback();
-                }
-            );
-        },
-        function (err) {
-            if (err)
-                console.log('failed');
+                var json = JSON.stringify(searchJSON);
+                console.log(json);
 
-            var json = JSON.stringify(searchJSON);
-            console.log(json);
-
-            fs.writeFile('output.json', json);
-        }
-    );
-});
+                fs.writeFile('output.json', json);
+            }
+        );
+    });
+}
 
 function _addHeading(text) {
     var heading = _removeExtraWhiteSpace(text);
@@ -162,7 +167,7 @@ function _addHeading(text) {
     }
 
     gbag.data.headings_link_map[property] = link;
-    currentHeading = heading;
+    currentHeadingLink = link;
 }
 
 function _removeTagsAndExtraWhiteSpace(string) {
@@ -193,3 +198,83 @@ function _parseFileMD(bag, next) {
         return next();
     });
 }
+
+var propertyMap = new Object();
+
+function parseMkDocsYml() {
+    try {
+        const config = yaml.safeLoad(fs.readFileSync('docs/mkdocs.yml', 'utf8'));
+        const indentedJson = JSON.stringify(config, null, 4);
+        console.log(indentedJson);
+        iterate(config, '')
+    } catch (e) {
+        console.log(e);
+    }
+
+    async.forEach(Object.keys(propertyMap), function (key, callback){
+        console.log(key);
+
+        var fileName = "./docs/sources/" + key;
+        writeToStartOfFile(fileName, propertyMap[key]);
+
+        // tell async that that particular element of the iterator is done
+        callback();
+
+    }, function(err) {
+        console.log('iterating done');
+    });
+}
+
+function writeToStartOfFile(file, pageTitle) {
+    var data = fs.readFileSync(file, "utf-8"); //read existing contents into data
+
+    if (data.indexOf("page_main_title") == -1) {
+
+        var buffer = new Buffer("page_main_title: " + pageTitle) + "\n";
+        var wstream = fs.createWriteStream(file);
+
+        wstream.on("finish", function() {
+        });
+
+        wstream.write(buffer);
+        wstream.write(data);
+        wstream.end();
+    }
+}
+
+function iterate(obj, stack) {
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+                iterate(obj[property], stack + '.' + property);
+            } else {
+                var value = obj[property].toString();
+                if (value.indexOf('.md') > 0) {
+                    propertyMap[value.trim()] = property.toString();
+                }
+            }
+        }
+    }
+}
+
+var prop = null;
+function iterateObject(obj) {
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+                if (obj[property].hasOwnProperty("matchLevel")
+                    && obj[property].matchLevel === 'full') {
+                    prop = property;
+                    return obj[property];
+                }  else {
+                        var val = iterateObject(obj[property]);
+                        if (val) {
+                            return val;
+                        }
+                }
+            }
+        }
+    }
+}
+
+parseDocsYml();
